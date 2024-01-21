90a5cfb750a833f0afc37e8ff8c00d7815bd8ea0
mayurkukade <mayurkukade@opticalarc.com>
2024-01-21 21:54:29 +0530

    add persronal details section

--- src/components/multistepForms/PersonalDetails.jsx
+++ src/components/multistepForms/PersonalDetails.jsx
@@ -1,6 +1,5 @@
 import { Button } from "@material-tailwind/react";
 import React from "react";
-import { ImCamera } from "react-icons/im";
 
 import {
   Input,
@@ -19,7 +18,10 @@ import { DayPicker } from "react-day-picker";
 import { ChevronRightIcon, ChevronLeftIcon } from "@heroicons/react/24/outline";
 import { useCountries } from "use-react-countries";
 import { state } from "../../data/StateIndia.js";
-
+import { industries } from "../../data/Industry.js";
+import { noticePeriodDays } from "../../data/Notice.js";
+import { experienceDurations } from "../../data/Experiance.js";
+import { annualSalaryRanges } from "../../data/AnnualSalary.js";
 const PersonalDetails = () => {
   const [date, setDate] = React.useState();
 
@@ -27,11 +29,9 @@ const PersonalDetails = () => {
   const [country, setCountry] = React.useState(221);
 
   const { name, flags, countryCallingCode } = countries[country];
-  console.log(state);
+  
   const dateResult = date ? format(date, "MM-dd-yy") : null;
-  console.log(dateResult);
-
-  console.log(date ? format(date, "PPP") : null);
+  
   return (
     <>
       <p className="text-xl w-auto bg-gray-400 p-2 ">Personal Details</p>
@@ -45,8 +45,7 @@ const PersonalDetails = () => {
           <p>Preferred Location</p>
         </div>
         <div className="md:space-y-8 space-y-4 col-span-2 ">
-        <ImCamera />
-        <input type="file" accept="image/*"  />
+          <input type="file" accept="image/*" />
           <Input label="Name" />
           <select className="mt-1 p-2 border  border-gray-400 text-gray-700 rounded-md w-full">
             <option>Gender</option>
@@ -216,66 +215,37 @@ const PersonalDetails = () => {
               }}
             />
           </div>
-          <Input label="Name" />
+          <select className="mt-1 p-2 border  border-gray-400 text-gray-700 rounded-md w-full overflow-y-auto">
+            {industries.map((industrie, index) => {
+              return <option key={index}>{industrie}</option>;
+            })}
+          </select>
           <select className="mt-1 p-2 border  border-gray-400 text-gray-700 rounded-md w-full overflow-y-auto">
             <option>Male</option>
             <option>Female</option>
           </select>
 
-          <Popover placement="bottom">
-            <PopoverHandler>
-              <Input
-                label="Select a Date"
-                onChange={() => null}
-                value={date ? format(date, "PPP") : ""}
-              />
-            </PopoverHandler>
-            <PopoverContent>
-              <DayPicker
-                mode="single"
-                selected={date}
-                onSelect={setDate}
-                showOutsideDays
-                className="border-0"
-                classNames={{
-                  caption:
-                    "flex justify-center py-2 mb-4 relative items-center",
-                  caption_label: "text-sm font-medium text-gray-900",
-                  nav: "flex items-center",
-                  nav_button:
-                    "h-6 w-6 bg-transparent hover:bg-blue-gray-50 p-1 rounded-md transition-colors duration-300",
-                  nav_button_previous: "absolute left-1.5",
-                  nav_button_next: "absolute right-1.5",
-                  table: "w-full border-collapse",
-                  head_row: "flex font-medium text-gray-900",
-                  head_cell: "m-0.5 w-9 font-normal text-sm",
-                  row: "flex w-full mt-2",
-                  cell: "text-gray-600 rounded-md h-9 w-9 text-center text-sm p-0 m-0.5 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-gray-900/20 [&:has([aria-selected].day-outside)]:text-white [&:has([aria-selected])]:bg-gray-900/50 first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
-                  day: "h-9 w-9 p-0 font-normal",
-                  day_range_end: "day-range-end",
-                  day_selected:
-                    "rounded-md bg-gray-900 text-white hover:bg-gray-900 hover:text-white focus:bg-gray-900 focus:text-white",
-                  day_today: "rounded-md bg-gray-200 text-gray-900",
-                  day_outside:
-                    "day-outside text-gray-500 opacity-50 aria-selected:bg-gray-500 aria-selected:text-gray-900 aria-selected:bg-opacity-10",
-                  day_disabled: "text-gray-500 opacity-50",
-                  day_hidden: "invisible",
-                }}
-                components={{
-                  IconLeft: ({ ...props }) => (
-                    <ChevronLeftIcon {...props} className="h-4 w-4 stroke-2" />
-                  ),
-                  IconRight: ({ ...props }) => (
-                    <ChevronRightIcon {...props} className="h-4 w-4 stroke-2" />
-                  ),
-                }}
-              />
-            </PopoverContent>
-          </Popover>
+          <select className="mt-1 p-2 border  border-gray-400 text-gray-700 rounded-md w-full overflow-y-auto">
+            {noticePeriodDays.map((notice, index) => {
+              return <option key={index}>{notice}</option>;
+            })}
+          </select>
 
-          <Input label="Current Location" />
-          <Input label="Preferred Location" />
-          <Input label="Preferred Location" />
+          <select className="mt-1 p-2 border  border-gray-400 text-gray-700 rounded-md w-full overflow-x-auto">
+            {experienceDurations.map((experienceDuration, index) => {
+              return <option key={index}>{experienceDuration}</option>;
+            })}
+          </select>
+          <select className="mt-1 p-2 border  border-gray-400 text-gray-700 rounded-md w-full overflow-x-auto">
+            {annualSalaryRanges.map((experienceDuration, index) => {
+              return <option key={index}>{experienceDuration}</option>;
+            })}
+          </select>
+          <select className="mt-1 p-2 border  border-gray-400 text-gray-700 rounded-md w-full overflow-x-auto">
+            {annualSalaryRanges.map((experienceDuration, index) => {
+              return <option key={index}>{experienceDuration}</option>;
+            })}
+          </select>
         </div>
       </div>
       <div className="flex justify-center">
